9e6a30cd85ff29e6b717a40cb7115161d0cc2bf4
池圣齐 <chi.shengqi@cgtn.com>
2021-10-27 23:19:11 +0800

    -

--- src/drop/index.js
+++ src/drop/index.js
@@ -1,96 +1,147 @@
 import { Card } from 'antd'
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
-
+import { useState } from 'react'
 import './index.styl'
+import { useEffect } from 'react';
 
-const getItemStyle = (isDragging, draggableStyle) => ({
-    // some basic styles to make the items look a bit nicer
-    userSelect: 'none',
-    padding: 8 * 2,
-    margin: `0 8px 0 0`,
 
-    // change background colour if dragging
-    background: isDragging ? 'lightgreen' : 'grey',
-
-    // styles we need to apply on draggables
-    ...draggableStyle,
-});
-
+const data = [
+    { id: 1, title: 1 },
+    { id: 2, title: 2 },
+    { id: 3, title: 3 },
+]
 
 function Drop() {
 
+    const onDragStart = (e) => {
+        console.log('onDragStart',e.target);
+    }
+    const onDragEnter = (e) => {
+        console.log('onDragEnter',e.target);
+    }
+     
     return (
         <div className={'card-drop'}>
-            <DragDropContext>
-                <Droppable droppableId="list"  direction="horizontal">
+            <div className={'droppable'}>
+                {
+                    data.map((item) => {
+                        const { title, id } = item;
+                        return (
+                            <Card 
+                            key={id} 
+                            title={id} 
+                            bordered={false} 
+                            className={'droppable-item'}
+                             draggable={'true'} 
+                             onDragStart={onDragStart}
+                             onDragEnter ={onDragEnter }
+                             >
+                                <p>{title}</p>
+                            </Card>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+
+
+function Drop2() {
+
+
+    const [state, setState] = useState(data);
+
+    const onDragEnd = (result) => {
+        const { source, destination } = result;
+
+        if (!destination) {
+            return;
+        }
+
+        if (destination.index === source.index) {
+            return;
+        }
+
+        const arr = Array.from(state);
+        const [removed] = arr.splice(result.source.index, 1);
+        arr.splice(result.destination.index, 0, removed);
+
+
+        setState(arr);
+    }
+
+    useEffect(() => {
+
+        window.resize = () => {
+
+        }
+
+    }, [])
+
+    return (
+        <div className={'card-drop'}>
+            <DragDropContext onDragEnd={onDragEnd}>
+                {/* 指定可被拖动放入的区域 */}
+                <Droppable droppableId="list" direction="horizontal">
                     {provided => (
                         <div ref={provided.innerRef} {...provided.droppableProps} className={'droppable'}>
+                            {
+                                state.map((item, index) => {
+                                    const { id, title } = item;
+                                    return (
+                                        <Draggable draggableId={id + ''} index={index} key={id}>
+                                            {(provided, snapshot) => (
+                                                <div
+                                                    ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}
+                                                    className={'droppable-item'}
+                                                >
+                                                    <Card title="Card title" bordered={false} style={{ width: 300 }}>
+                                                        <p>{title}</p>
+                                                    </Card>
+                                                </div>
+                                            )}
+                                        </Draggable>
+                                    )
+                                })
+                            }
 
-                            <Draggable draggableId={'1'} index={1}>
-                                {(provided, snapshot) => (
-                                    <div
-                                        ref={provided.innerRef}
-                                        {...provided.draggableProps}
-                                        {...provided.dragHandleProps}
-                                    >
-                                        <Card title="Card title" bordered={false} style={{ width: 300 }}>
-                                            <p>Card content</p>
-                                            <p>Card content</p>
-                                            <p>Card content</p>
-                                        </Card>
-                                    </div>
-                                )}
-                            </Draggable>
-                            <Draggable draggableId={'2'} index={2}>
-                                {(provided, snapshot) => (
-                                    <div
-                                        ref={provided.innerRef}
-                                        {...provided.draggableProps}
-                                        {...provided.dragHandleProps}
-                                    >
-                                        <Card title="Card title" bordered={false} style={{ width: 300 }}>
-                                            <p>Card content</p>
-                                            <p>Card content</p>
-                                            <p>Card content</p>
-                                        </Card>
-                                    </div>
-                                )}
-                            </Draggable>
-                            <Draggable draggableId={'3'} index={3}>
-                                {(provided, snapshot) => (
-                                    <div
-                                        ref={provided.innerRef}
-                                        {...provided.draggableProps}
-                                        {...provided.dragHandleProps}
-                                    >
-                                        <Card title="Card title" bordered={false} style={{ width: 300 }}>
-                                            <p>Card content</p>
-                                            <p>Card content</p>
-                                            <p>Card content</p>
-                                        </Card>
-                                    </div>
-                                )}
-                            </Draggable>
-                            <Draggable draggableId={'4'} index={4}>
-                                {(provided, snapshot) => (
-                                    <div
-                                        ref={provided.innerRef}
-                                        {...provided.draggableProps}
-                                        {...provided.dragHandleProps}
-                                    >
-                                        <Card title="Card title" bordered={false} style={{ width: 300 }}>
-                                            <p>Card content</p>
-                                            <p>Card content</p>
-                                            <p>Card content</p>
-                                        </Card>
-                                    </div>
-                                )}
-                            </Draggable>
                             {provided.placeholder}
                         </div>
                     )}
                 </Droppable>
+                <Droppable droppableId="list2" direction="horizontal">
+                    {provided => (
+                        <div ref={provided.innerRef} {...provided.droppableProps} className={'droppable'}>
+                            {
+                                state.map((item, index) => {
+                                    const { id, title } = item;
+                                    return (
+                                        <Draggable draggableId={id + '1'} index={index} key={id}>
+                                            {(provided, snapshot) => (
+                                                <div
+                                                    ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}
+                                                    className={'droppable-item'}
+                                                >
+                                                    <Card title="Card title" bordered={false} style={{ width: 300 }}>
+                                                        <p>{title}</p>
+                                                    </Card>
+                                                </div>
+                                            )}
+                                        </Draggable>
+                                    )
+                                })
+                            }
 
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
 
             </DragDropContext>
 
