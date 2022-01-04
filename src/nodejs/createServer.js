39002141cb28bfca919497dbbc594a20e754e81a
池圣齐 <chi.shengqi@cgtn.com>
2022-01-04 18:04:54 +0800

    静态资源服务器

--- src/nodejs/createServer.js
+++ src/nodejs/createServer.js
@@ -1,11 +1,46 @@
 const http = require('http');
+const server = http.createServer();
+const { URL } = require('url');
+const fs = require('fs');
+const path = require('path')
 
-// 创建本地服务器来从其接收数据
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({
-        data: 'Hello World!'
-    }));
-});
 
-server.listen(8000);
+const CONTENT_TYPES_CONFIG = {
+    html: 'text/html',
+    css: 'text/css',
+    js: 'text/javascript',
+    png: 'image/png'
+};
+
+
+const staticPath = path.join(__dirname, 'static');
+const errorHtml = '<html>404</html>'
+
+server.on('request', (req, res) => {
+    //处理响应头
+    const reqURL = new URL(req.url, 'http://localhost/8080');
+    const { pathname } = reqURL;
+    let suffixName = pathname.split('.').pop().toLowerCase();
+    let ContentType = CONTENT_TYPES_CONFIG[suffixName];
+
+    res.statusCode = 200
+    res.setHeader({
+        'Content-Type': ContentType || CONTENT_TYPES_CONFIG['html']
+    });
+
+    if (!ContentType) {
+        res.end(errorHtml);
+    } else {
+
+        fs.readFile(path.join(staticPath, pathname), (err, data) => {
+            if (err) {
+                res.end(errorHtml)
+            }
+            res.end(data);
+        });
+    }
+
+})
+
+server.listen(8080);
+console.log("启动服务器：8080")
