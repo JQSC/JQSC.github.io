784f74991d846f942ce6512cdae818bcb5ccf153
池圣齐 <chi.shengqi@cgtn.com>
2022-02-08 00:02:40 +0800

    增加redux

--- src/components/redux/applyMiddleware.js
+++ src/components/redux/applyMiddleware.js
@@ -1,17 +1,19 @@
 // const { createStore } = require('redux');
 
 function logger({ getState }) {
-    return next => action => {
-        console.log('will dispatch', action)
+    return dispatch => {
+        return action => {
+            //console.log('will dispatch', action)
 
-        // 调用 middleware 链中下一个 middleware 的 dispatch。
-        const returnValue = next(action)
+            // 调用 middleware 链中下一个 middleware 的 dispatch。
+            const returnValue = dispatch(action)
 
-        console.log('state after dispatch', getState())
+            //console.log('state after dispatch', getState())
 
-        // 一般会是 action 本身，除非
-        // 后面的 middleware 修改了它。
-        return returnValue
+            // 一般会是 action 本身，除非
+            // 后面的 middleware 修改了它。
+            return returnValue
+        }
     }
 }
 
@@ -24,7 +26,7 @@ function applyMiddleware(middlewares) {
 
             const middlewareAPI = {
                 getState: store.getState,
-                dispatch: (...args) => dispatch(...args),
+                //dispatch: (...args) => dispatch(...args),
             }
 
             const chain = middlewares(middlewareAPI);
--- src/components/redux/index.js
+++ src/components/redux/index.js
@@ -7,7 +7,7 @@ const bindActionCreators = require('./bindActionCreator')
 let store = createStore();
 
 //订阅store的变化
-let unsubscribe = store.subscribe(() => console.log(store.getState()))
+//let unsubscribe = store.subscribe(() => console.log(store.getState()))
 
 
 //绑定action 和 dispatch
